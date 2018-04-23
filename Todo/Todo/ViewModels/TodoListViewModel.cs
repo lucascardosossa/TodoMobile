00b538b740b329fb2ca9b19c0e15a4a7058b4a93
Lucas Cardoso da Silva <kardoso@gmail.com>
2018-04-22 21:12:27 -0300

    primeiro commit

---
+++ Todo/Todo/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Prism.Navigation;
+
+namespace Todo.ViewModels
+{
+    public class TodoListViewModel : ViewModelBase
+    {
+        public TodoListViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            
+        }
+    }
+}
